ccfd0eecbfec8e685b735aa44976e84faa74bc22
AberrantCurse <70731395+AberrantCurse@users.noreply.github.com>
2022-05-18 22:23:42 +1000

    Add files via upload

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php
+$message = $_POST['message'];
+
+$email_from = 'technogamedevs@gmail.com';
+$email_subject = 'New Commision!';
+$email_body = $message;
+$to = "technogamedevs@gmail.com";
+$headers = "From: $email_from /r /n";
+mail($to,$email_subject,$email_body,$headers);
+?>
+
